0204857f291bcb66e804f4d8daf679c0990c4675
JEPTUI <alicejeptui@gmail.com>
2023-08-06 07:48:42 -0700

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$('document').ready(function () {
+  let amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+
+$.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+	$('#api_status').addClass('available');
+      } else {
+	$('#api_status').removeClass('available');
+      }
+    }
+  });
+
+});
